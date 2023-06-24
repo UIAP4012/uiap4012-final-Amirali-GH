ad1dac423feb408b6ea2d46d7160f0a0a24daf7d
amirali <amiralighadiri1383@gmail.com>
2023-06-24 21:29:07 +0430

    Start the programing

---
+++ Store.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//======================================================================================
+//Classes:
+
+//======================================================================================
+//Prototypes:
+void spendtime();
+
+//======================================================================================
+// main function:
+int main(){
+    cout<<"***********    Hello my friend, Welcome to your Mall    ***********"<<endl;
+    cout<<"\nLets go to set our initial setting and storage...\n";
+    spendtime();spendtime();
+    system("cls");
+}
+
+
+void spendtime() {
+    for (int i = 0; i < 1000000000; i++){}
+}
+
+
+
